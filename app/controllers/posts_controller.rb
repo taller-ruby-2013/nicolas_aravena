a63570b60d6ac44f889aa6b18c53a4860e2e471b
Nicolas Aravena <nicolas@aravenas.com>
2013-12-16 18:58:49 -0300

    Clase 5 version rapida

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,46 @@
 class PostsController < ApplicationController
-    def new
-    end
-    def create
-    	@post = Post.new(post_params)
+	def new 
+		@post = Post.new
+	end
 
-    	@post.save
-    	redirect_to @post
+    def create
+    	@post = Post.new(params[:post].permit(:title, :text))
+
+    	if @post.save
+    		redirect_to @post
+    	else
+    		render 'new'
+    	end
     end
+
     def show
     	@post = Post.find(params[:id])
+        @comentario = Comment.new
     end
+
     def index 
     	@posts = Post.all
     end
+
+    def edit
+    	@post = Post.find(params[:id])
+    end
+
+    def update
+    	@post = Post.find(params[:id])
+    	if @post.update(params[:post].permit(:title, :text))
+    		redirect_to @post
+    	else
+    		render 'edit'
+    	end
+    end
+
+    def destroy
+    	@post = Post.find(params[:id])
+    	@post.destroy
+    	redirect_to posts_path
+    end
+
     private
     	def post_params
     		params.require(:post).permit(:title, :text)

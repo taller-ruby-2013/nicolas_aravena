b00534e9fb51c39cbc7ed20926ca1ecbd82c788e
Nicolas Aravena <nicolas@aravenas.com>
2014-01-09 22:14:23 -0300

    ivan la masca

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-  	@posts = Post.find(:all, :order => "created_at")
+  	@posts = Post.find(:all, :order => "created_at DESC")
   end
 end
